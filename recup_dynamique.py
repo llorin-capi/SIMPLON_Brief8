2f9e93406bdeb292155abb67ae8c8671d925e330
Léa <lealorin51@gmail.com>
2024-10-14 19:34:56 +0200

    Update recup_dynamique.py

--- recup_dynamique.py
+++ recup_dynamique.py
@@ -1,25 +1,6 @@
 import requests
 from services import jcdecaux as jcd
 
-def recup_stats_station(num_station):
-    url= jcd.api_base_url + "stations/" + str(num_station) +"?contract=" + jcd.contrat + "&apiKey=" + jcd.key
-    response = requests.get(url)
-    if response.status_code == 200: #Test de la réponse
-        stat_station = response.json()
-        station={
-            "numero" : stat_station['number'],
-            "nom" : stat_station['name'],
-            "latitude" : stat_station['position']['latitude'],
-            "longitude" : stat_station['position']['longitude'],
-            "statut" : stat_station['status'],
-            "capacite" : stat_station['mainStands']['capacity'],
-            "dispo_velos" : stat_station['mainStands']['availabilities']['bikes'],
-            "dispo_stands" : stat_station['mainStands']['availabilities']['stands']
-        }
-        return station
-    else:
-        print("Récup stats station :", num_station, "- Erreur:", response.status_code)
-
 def recup_stations():
     url=jcd.api_base_url + "stations?contract="+ jcd.contrat + "&apiKey=" + jcd.key
     response = requests.get(url)
