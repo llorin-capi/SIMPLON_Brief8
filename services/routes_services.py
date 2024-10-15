446d1f3539946d3f3fd64a539e0a003396a78e3f
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-15 12:20:47 +0200

    Add route to display map with JCDecaux stations.
    This commit introduces a route that displays a map centered on the default contract city with JCDecaux bike stations. The map is rendered using coordinates fetched from the maps service and bike station data from the JCDecaux service.

--- services/routes_services.py
+++ services/routes_services.py
@@ -1,2 +1,18 @@
+from services import jcdecaux_services as jcds
+from services import maps_services as maps
+
+
 def register_routes(app):
-    pass
+    """
+    :param app: The application instance to which routes will be registered.
+    :return: None
+
+    """
+    @app.route('/')
+    def show_map_stations():
+        city_stations = jcds.get_stations()
+        city_latitude, city_longitude = maps.fetch_gps_coordinates(jcds.DEFAULT_CONTRACT_CITY)
+        city_centered_map = maps.get_centered_map(city_latitude, city_longitude)
+        city_centered_map = maps.add_locations_to_map(city_centered_map, city_stations)
+
+        return city_centered_map.get_root().render()
