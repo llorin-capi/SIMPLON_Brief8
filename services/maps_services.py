62402787bba7bd9919925a43b643e562a0a40ca0
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-15 14:08:14 +0200

    Refactor map service to use OSMnx for graph retrieval
    Replaced geopy with OSMnx to improve the efficiency of graph retrieval and map creation processes. Simplified the function for fetching GPS coordinates and added new utility functions for working with OSMnx data.

--- services/maps_services.py
+++ services/maps_services.py
@@ -1,52 +1,66 @@
 import folium
-import geopy
-import certifi
-import ssl
-from geopy.geocoders import Nominatim
+import osmnx
 from typing import Tuple, Dict, List
 
 DEFAULT_COUNTRY = "France"
+INITIAL_ZOOM_LEVEL = 14
 
 
-def setup_ssl_context() -> None:
+def get_osmnx_graph(city_location: str):
     """
-    Sets up the SSL context for geopy to use a default context with a specified certificate file.
+    :param city_location: The geographical location in the form of a string, representing the city or place for which the OSMnx graph is to be retrieved.
+    :return: A graph object representing the street network of the specified city or place, which is obtained using OSMnx's graph_from_place function.
+    """
+    return osmnx.graph_from_place(city_location, network_type="drive")
 
-    This function creates a default SSL context using certifi's CA bundle and assigns it to geopy's default SSL context.
 
+def get_city_center(edges) -> List[float]:
+    """
+    :param edges: A GeoDataFrame containing the geographical edge data.
+    :return: A list containing the latitude and longitude of the centroid of the union of all edges.
+    """
+    return [edges.unary_union.centroid.y, edges.unary_union.centroid.x]
+
+
+def create_folium_map(city_center: List[float]) -> folium.Map:
+    """
+    :param city_center: A list of two floats representing the latitude and longitude of the city center.
+    :return: A Folium map instance centered at the given city coordinates with an initial zoom level.
+    """
+    return folium.Map(location=city_center, zoom_start=INITIAL_ZOOM_LEVEL)
+
+
+def add_geojson_to_map(edges, map_object: folium.Map) -> None:
+    """
+    :param edges: A GeoJSON object or file path representing the geographic features to be added to the map.
+    :param map_object: A folium.Map object to which the GeoJSON features will be added.
     :return: None
     """
-    ssl_context = ssl.create_default_context(cafile=certifi.where())
-    geopy.geocoders.options.default_ssl_context = ssl_context
+    folium.GeoJson(edges).add_to(map_object)
 
 
-def fetch_gps_coordinates(city: str, country: str = DEFAULT_COUNTRY) -> Tuple[float, float]:
+def get_centered_map(city: str, country: str = DEFAULT_COUNTRY) -> folium.Map:
     """
-    :param city: The name of the city for which GPS coordinates are to be fetched.
-    :param country: The name of the country where the city is located. Defaults to DEFAULT_COUNTRY.
-    :return: A tuple containing the latitude and longitude of the specified location.
+    :param city: Name of the city for which the map is to be centered.
+    :param country: Name of the country in which the city is located, defaulting to DEFAULT_COUNTRY.
+    :return: A folium Map object centered on the given city.
     """
-    setup_ssl_context()
-    geolocator = Nominatim(user_agent="my_geocoder")
-    location_str = f"{city}, {country}"
-    location = geolocator.geocode(location_str)
-    return location.latitude, location.longitude
+    city_location = f"{city}, {country}"
+    city_graph = get_osmnx_graph(city_location)
+    nodes, edges = osmnx.graph_to_gdfs(city_graph)
 
+    city_center = get_city_center(edges)
+    centered_map = create_folium_map(city_center)
 
-def get_centered_map(latitude: float, longitude: float) -> folium.Map:
-    """
-    :param latitude: The latitude coordinate for the center of the map.
-    :param longitude: The longitude coordinate for the center of the map.
-    :return: A Folium Map object centered at the provided latitude and longitude coordinates.
-    """
-    map_center = [latitude, longitude]
-    centered_map = folium.Map(location=map_center, zoom_start=14)
+    #add_geojson_to_map(edges, centered_map)
+
     return centered_map
 
-def get_occupation_color(score: float) -> str :
+
+def get_occupation_color(score: float) -> str:
     """
-    :param score: A float representing the occupation score
-    :return: A string representing the corresponding color based on the occupation score
+    :param score: A floating-point number representing a score.
+    :return: A string representing a color based on the score's value.
     """
     match score:
         case _ if score < 0.15:
@@ -61,9 +75,9 @@ def get_occupation_color(score: float) -> str :
 
 def add_locations_to_map(map_object: folium.Map, locations: List[Dict[str, any]]) -> folium.Map:
     """
-    :param map_object: The folium.Map object to which the locations will be added.
-    :param locations: A list of dictionaries where each dictionary contains 'dispo_velos', 'capacite', 'latitude', 'longitude', and 'nom' keys representing the properties of each location.
-    :return: The folium.Map object with the locations added.
+    :param map_object: The map object to which locations will be added
+    :param locations: A list of dictionaries, each representing a location with latitude, longitude, name, dispo_velos, and capacite keys
+    :return: The updated folium Map object with added location markers
     """
     for location in locations:
         occupation = location['dispo_velos'] / location['capacite']
@@ -72,5 +86,4 @@ def add_locations_to_map(map_object: folium.Map, locations: List[Dict[str, any]]
             popup=location["nom"],
             icon=folium.Icon(color=get_occupation_color(occupation))
         ).add_to(map_object)
-
     return map_object
