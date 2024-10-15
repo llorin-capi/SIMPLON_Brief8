21dd98f2f09d363c3dd0f0a78717ec39a8b4547d
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-15 10:37:24 +0200

    Enhance location markers and SSL context setup
    Updated the SSL context setup function for clarity and added a color-coded occupation marker feature to the map based on bike availability scores. Additionally, enhanced the parameter descriptions for better readability.

--- services/maps_services.py
+++ services/maps_services.py
@@ -8,9 +8,11 @@ from typing import Tuple, Dict, List
 
 def setup_ssl_context() -> None:
     """
-    Sets up the SSL context required for secure communication by creating a default context
-    using certificates from `certifi`. The context is then set as the default SSL context
-    for `geopy.geocoders`.
+    Sets up the SSL context for secure connections.
+
+    This function initializes the default SSL context with the Certification Authority
+    (CA) certificates provided by the certifi package. It then assigns this context
+    to the default SSL context used by geopy geocoders.
 
     :return: None
     """
@@ -21,8 +23,8 @@ def setup_ssl_context() -> None:
 def fetch_gps_coordinates(city: str, country: str = "France") -> Tuple[float, float]:
     """
     :param city: Name of the city to fetch GPS coordinates for.
-    :param country: Name of the country, defaults to "France".
-    :return: Tuple containing the latitude and longitude of the city.
+    :param country: Name of the country to fetch GPS coordinates for, default is "France".
+    :return: A tuple containing the latitude and longitude of the specified location.
     """
     setup_ssl_context()
     geolocator = Nominatim(user_agent="my_geocoder")
@@ -33,24 +35,41 @@ def fetch_gps_coordinates(city: str, country: str = "France") -> Tuple[float, fl
 
 def get_centered_map(latitude: float, longitude: float) -> folium.Map:
     """
-    :param latitude: Latitude coordinate around which the map should be centered.
-    :param longitude: Longitude coordinate around which the map should be centered.
-    :return: A Folium map object centered at the specified latitude and longitude.
+    :param latitude: Latitude coordinate for centering the map.
+    :param longitude: Longitude coordinate for centering the map.
+    :return: A Folium Map centered at the provided latitude and longitude.
     """
     map_center = [latitude, longitude]
     centered_map = folium.Map(location=map_center, zoom_start=14)
     return centered_map
 
+def get_occupation_color(score):
+    """
+    :param score: A floating point number representing the score of the occupation.
+    :return: A string representing the color associated with the given score. The color can be 'red' if the score is less than 0.15, 'orange' if the score is between 0.15 and 0.35, 'green' if the score is between 0.35 and 0.70, and 'purple' if the score is 0.70 or higher.
+    """
+    match score:
+        case _ if score < 0.15:
+            return 'red'
+        case _ if 0.15 <= score < 0.35:
+            return 'orange'
+        case _ if 0.35 <= score < 0.70:
+            return 'green'
+        case _:
+            return 'purple'
+
 
 def add_locations_to_map(map_object: folium.Map, locations: List[Dict[str, any]]) -> None:
     """
-    :param map_object: The folium Map object to which the locations will be added.
-    :param locations: A list of dictionaries, each containing the latitude, longitude, and name of a location to be added to the map.
+    :param map_object: The Folium map object to which the locations will be added.
+    :param locations: A list of dictionaries, each containing the keys 'dispo_velos', 'capacite', 'latitude', 'longitude', and 'nom'.
     :return: None
     """
     for location in locations:
+        occupation = location['dispo_velos'] / location['capacite']
         folium.Marker(
             location=[location["latitude"], location["longitude"]],
             popup=location["nom"],
-        ).add_to(map_object)
+            icon = folium.Icon(color=get_occupation_color(occupation)
+        ).add_to(map_object))
     map_object.save('map_locations.html')
